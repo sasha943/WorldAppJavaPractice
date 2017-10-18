0af049195fae82dac3d94d84a80009719107d0a2
lev.romanets <lev.romanets@worldapp.com>
2017-10-18 15:59:36 +0300

    ArrayMinMaxValue done

--- src/main/java/lev/ArrayMinMaxValue.java
+++ src/main/java/lev/ArrayMinMaxValue.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayMinMaxValue{
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input array length: ");
+        int n = input.nextInt(); //Array length
+        int[] array1 = new int[n];
+        for (int i=0; i<n; i++){
+            array1[i] = (int)(Math.random() * 100);
+        }
+        System.out.println("Input array is \n" + Arrays.toString(array1));
+        System.out.println();
+
+        int min = array1[0];
+        int max = array1[0];
+        for (int i=1; i<n; i++){
+            if (max < array1[i]) max = array1[i];
+            if (min > array1[i]) min = array1[i];
+        }
+
+        System.out.println("Min value: " + min);
+        System.out.println("Max value: " + max);
     }
 }
