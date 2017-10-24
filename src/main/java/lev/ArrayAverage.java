c4c2e6a635e5b4f6bd02b47ebc09325c3fda5b1b
lev.romanets <lev.romanets@worldapp.com>
2017-10-24 10:24:06 +0300

    small fixes

--- src/main/java/lev/ArrayAverage.java
+++ src/main/java/lev/ArrayAverage.java
@@ -7,19 +7,18 @@ public class ArrayAverage {
 
         int arrayLength = 6;
         int maxValue = 100;
-        float avg = 0;
+        float sum = 0;
 
-        int[] array1 = new int[arrayLength];            //creating random array
-        for (int i=0; i<array1.length; i++){
-            array1[i] = (int)(Math.random() * maxValue);
+        int[] randomArray = new int[arrayLength];            //creating random array
+        for (int i=0; i<randomArray.length; i++){
+            randomArray[i] = (int)(Math.random() * maxValue);
         }
-        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(randomArray));
 
-        for (int i=0; i<array1.length; i++){            //calculation average of array
-            avg += array1[i];
+        for (int i = 0; i < randomArray.length; i++){            //calculation sum of array values
+            sum += randomArray[i];
         }
-        avg = avg / array1.length;
 
-        System.out.println("Average of array = " + avg);
+        System.out.println("Average of array = " + sum / randomArray.length);
     }
 }
--- src/main/java/lev/ArrayCopy.java
+++ src/main/java/lev/ArrayCopy.java
@@ -7,16 +7,16 @@ public class ArrayCopy {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Input array length: ");
-        int n = input.nextInt(); //Array length
-        int[] array1 = new int[n];
-        for (int i = 0; i < n; i++){
+        int arrayLength = input.nextInt(); //Array length
+        int[] array1 = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++){
             array1[i] = (int)(Math.random() * 100);
         }
-        int[] array2 = new int[n];
+        int[] array2 = new int[arrayLength];
         System.out.println("Input array = " + Arrays.toString(array1));
         System.out.println("Second array = " + Arrays.toString(array2));
 
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < arrayLength; i++){
             array2[i] = array1[i];
         }
         System.out.println("Copy first array to second: \n" + Arrays.toString(array2));
