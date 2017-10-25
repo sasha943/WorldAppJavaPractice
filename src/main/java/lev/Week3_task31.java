63dda330e7eda2f2a45f9ae301c9fccc6f9c403b
lev.romanets <lev.romanets@worldapp.com>
2017-10-25 18:02:01 +0300

    week3_task31 in progress

---
+++ src/main/java/lev/Week3_task31.java
@@ -0,0 +1,10 @@
+// 3.1. Заменить все числа, которые делятся на 5 без остачи в двухмерной матрице на 8
+
+public class Week3_task31 {
+    public static void main(String[] args) {
+
+    }
+    public static int[][] generateMarix(int size, int minValue, int maxValue){
+
+    }
+}
