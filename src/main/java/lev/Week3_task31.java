cea45e66faf9e0c0f24365d45b617caac84ed329
lev <lev.romanets@worldapp.com>
2017-10-25 23:21:13 +0300

    Заменить все числа, которые делятся на 5 без остачи в двухмерной матрице на 8

--- src/main/java/lev/Week3_task31.java
+++ src/main/java/lev/Week3_task31.java
@@ -3,8 +3,42 @@
 public class Week3_task31 {
     public static void main(String[] args) {
 
-    }
-    public static int[][] generateMarix(int size, int minValue, int maxValue){
+        int[][] matrix = generateMarix(3, 5, 10, 99);
 
+        System.out.println("Givet array:");
+        printMatrix(matrix);
+
+        System.out.println("\nAll multiple 5 values replaced by 8");
+        printMatrix(replaceM5by8(matrix));
+
+
+    }
+    public static int[][] generateMarix(int rows, int columns, int minValue, int maxValue){
+        int[][] matrix = new int[rows][columns];
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                matrix[i][j] = minValue + (int)(Math.random() * (maxValue - minValue));
+            }
+        }
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[i].length; j++){
+                if (j > 0) System.out.print(" ");
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] replaceM5by8(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[i].length; j++){
+                if (matrix[i][j] % 5 == 0) matrix[i][j] = 8;
+            }
+        }
+        return matrix;
     }
 }
