2ac61ecaa497ac3c8bfcdecc0d2968ab621dcb13
lev.romanets <lev.romanets@worldapp.com>
2017-11-07 13:19:49 +0200

    task 3.6 refactoring

--- src/main/java/lev/Week3_task36.java
+++ src/main/java/lev/Week3_task36.java
@@ -3,57 +3,68 @@
 public class Week3_task36 {
     public static void main(String[] args) {
 
+        String strRandomBinaryNumber = randomStringOfOnesAndZeros( 16 );
+        //strRandomBinaryNumber = "00011";
+
+        System.out.println(strRandomBinaryNumber);
+        System.out.println();
+
+        char char2find = '1';
+        int[] chain = findChain( strRandomBinaryNumber, char2find );
+        System.out.println("Max " + char2find + "-chain starts at position " + chain[0] + " and counts " + chain[1] + " symbols");
+
+        char2find = '0';
+        chain = findChain( strRandomBinaryNumber, char2find );
+        System.out.println("Max " + char2find + "-chain starts at position " + chain[0] + " and counts " + chain[1] + " symbols");
+
+    }
+
+    public static int[] findChain(String str, char char2find){
+
+        int[] chain = new int[2];
+        int maxChainStartIndex = 0;
+        boolean chainFlag = false;
+        int chainCount = 0;
+        int maxChars = 0;
+
+        if (str.charAt(0) == char2find){
+            chainFlag = true;
+            chainCount = 1;
+        }
+
+        for (int i = 1; i < str.length(); i++){
+            if (str.charAt(i) == char2find){
+                if (chainFlag){
+                    chainCount++;
+                }
+                else{
+                    chainFlag = true;
+                    chainCount = 1;
+                }
+            }
+            else{
+                if (chainFlag){
+                    chainCount = 0;
+                }
+                chainFlag = false;
+            }
+            if (chainCount > maxChars){
+                maxChainStartIndex = i - chainCount + 1;
+                maxChars = chainCount;
+            }
+        }
+        chain[0] = maxChainStartIndex;
+        chain[1] = maxChars;
+        return chain;
+    }
+
+
+    public static String randomStringOfOnesAndZeros(int lengthOfOnesAndZeros){
         String strRandomBinaryNumber = "";
-        int lengthOfOnesAndZeros = 12;
         for (int i = 0; i < lengthOfOnesAndZeros; i++){
             if (Math.random() > 0.5) strRandomBinaryNumber += "1";
             else strRandomBinaryNumber += "0";
         }
-
-        System.out.println(strRandomBinaryNumber);
-        int maxOnesStartIndex = 0;
-        int maxZerosStartIndex = 0;
-        boolean onesFlag;
-        int onesCount = 0;
-        int zerosCount = 0;
-        int maxOnes = 0;
-        int maxZeros = 0;
-
-        if (strRandomBinaryNumber.charAt(0) == '1'){
-            onesFlag = true;
-            onesCount = 1;
-        }
-        else
-        {
-            onesFlag = false;
-            zerosCount = 1;
-        }
-
-        for (int i = 1; i < strRandomBinaryNumber.length(); i++){
-            if (strRandomBinaryNumber.charAt(i) == '1'){
-                if (!onesFlag){
-                    if (zerosCount > maxZeros){
-                        maxZerosStartIndex = i - zerosCount;
-                        maxZeros = zerosCount;
-                    }
-                    zerosCount = 0;
-                }
-                onesCount++;
-                onesFlag = true;
-            }
-            else{
-                if (onesFlag){
-                    if (onesCount > maxOnes){
-                        maxOnesStartIndex = i - onesCount;
-                        maxOnes = onesCount;
-                    }
-                    onesCount = 0;
-                }
-                zerosCount++;
-                onesFlag = false;
-            }
-        }
-        System.out.println("Max Ones chain starts at position " + maxOnesStartIndex + " and counts " + maxOnes + " ones");
-        System.out.println("Max Zeros chain starts at position " + maxZerosStartIndex + " and counts " + maxZeros + " zeros");
+        return strRandomBinaryNumber;
     }
 }
